824ac48312b2f5d92a6e2dac097ba547bc22bc23
strikerJava <rtkanall@gmail.com>
2018-10-08 20:44:29 -0400

    Three Functions
    Testing and learning str, len, float.

---
+++ python/AutomateTheBoringStuff/Chapter1/threeFunc.py
@@ -0,0 +1,6 @@
+#severalFunctions, str int & float
+
+print('It\'s over ' + str(9001))
+print('lenth of previous string: ' + str(len('It\'s over ' + str(9001)))) 
+#print(len('It\'s over ' + str(9001)))
+print('float function: ' + str(float('3.14')))
