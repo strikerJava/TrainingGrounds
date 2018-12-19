c1b9d1eb381a233c1770b34096d9cd2261df28bc
strikerJava <rtkanall@gmail.com>
2018-12-18 22:05:40 -0500

    Tonight's prog

--- python/reportingTool.py
+++ python/reportingTool.py
@@ -12,7 +12,7 @@ def getPopularArticles( ):
  c = db.cursor()
 # c.execute("SELECT SUBSTRING(path, 10 , LENGTH(path)-1), COUNT(path) FROM log WHERE path LIKE '/a%'  GROUP BY path ORDER BY COUNT(path) DESC;")
 
- c.execute("SELECT SUBSTRING(path, 10 , LENGTH(path)-1), COUNT(path) FROM log WHERE path LIKE '/a%'  GROUP BY path ORDER BY COUNT(path) DESC;")
+ c.execute("SELECT articles.title, COUNT(path) FROM articles, log WHERE articles.slug = (SUBSTRING(path, 10 , LENGTH(path)-1))  GROUP BY articles.title ORDER BY COUNT(path) DESC LIMIT 3;")
  #SELECT slug, title from articles; gets slug to match to path without
  #above gets the
  db.close()
@@ -21,7 +21,11 @@ def getPopularArticles( ):
 
 def getPopularAuthors( ):
  print("This is the popular Author Function")
- db = psycopg2.connect(database=DBNEWS)
+ #select COUNT(articles.author), authors.name  from articles, authors where articles.author = authors.id GROUP BY authors.name;
+ #above gets
+ # SELECT articles.author, COUNT(author) FROM articles, log WHERE articles.slug = (SUBSTRING(path, 10 , LENGTH(path)-1))  GROUP BY articles.author ORDER BY COUNT(path) DESC;
+ #gets count of author hits
+db = psycopg2.connect(database=DBNEWS)
  c = db.cursor()
  c.execute("")
 
